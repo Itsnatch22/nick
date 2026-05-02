1b028ce9908de9fb2574525246f2fc57674f8827
Itsnatch <kamaumark996@gmail.com>
2026-05-02 18:55:22 +0300

    nick

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +34,7 @@ export default function RootLayout({
         <TooltipProvider>
           {children}
           <Analytics />
+          <SpeedInsights />
         </TooltipProvider>
       </body>
     </html>
